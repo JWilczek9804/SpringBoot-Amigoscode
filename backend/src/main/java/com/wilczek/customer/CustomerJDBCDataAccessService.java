c1b2a009e17f57ecaa5b7b068f3f02d2dae543ca
JWilczek9804 <167343233+JWilczek9804@users.noreply.github.com>
2024-07-11 17:07:08 +0200

    break existPersonWithEmail method

--- backend/src/main/java/com/wilczek/customer/CustomerJDBCDataAccessService.java
+++ backend/src/main/java/com/wilczek/customer/CustomerJDBCDataAccessService.java
@@ -99,7 +99,7 @@ public class CustomerJDBCDataAccessService implements CustomerDAO {
         var sql = """
                 SELECT count(id)
                 FROM customer
-                WHERE email=?
+                WHERE name=?
                 """;
         Integer count = jdbcTemplate.queryForObject(sql,Integer.class,email);
         return count != null && count >0;
